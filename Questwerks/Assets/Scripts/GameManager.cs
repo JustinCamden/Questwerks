9b3f2b5639245521e5ab2977776009f40030ef50
Abhishek Biswas <biswasa@usc.edu>
2018-01-17 22:07:41 -0800

    Basic pause menu functional

---
+++ Questwerks/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	// True if game has ended
+	private bool gameOver;
+
+	// True if game is paused
+	public bool gamePaused;
+
+	// Use this for initialization
+	void Start () {
+		gameOver = false;
+		gamePaused = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gamePaused || gameOver) {
+			Time.timeScale = 0;
+		} else {
+			Time.timeScale = 1;
+		}		
+	}
+
+	public void endGame() {
+		gameOver = true;
+		Time.timeScale = 0;
+	}
+
+	public bool isGameOver() {
+		return gameOver;
+	}
+
+}
---
+++ Questwerks/Assets/Scripts/UIManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+
+	// Reference to GameManager instance
+	public GameManager gameManager;
+
+	// Reference to Main Menu object
+	public GameObject mainMenu;
+
+	// References to Text objects in menu
+	public Text pauseText;
+	public Text winText;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Check for game end state
+		if (gameManager.isGameOver()) {
+			pauseText.enabled = false;
+			winText.enabled = true;
+
+			return; // Don't accept further input!
+		}
+
+		// Key to open / close pause menu
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			gameManager.gamePaused = !gameManager.gamePaused;
+			mainMenu.SetActive(!mainMenu.activeInHierarchy);
+			pauseText.enabled = true;
+			winText.enabled = false;
+		}
+	}
+
+	public void QuitGame() {
+		Application.Quit();
+	}
+
+	public void RestartGame() {
+		EditorSceneManager.LoadScene(0);
+	}
+}
